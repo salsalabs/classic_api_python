d73b8c8b0f0462a3a2de54f7d79852dd1e4613ae
Allen Leonard <aleonard@salsalabs.com>
2018-08-15 14:10:40 -0500

    Example to read supporters and dump XML.

---
+++ read_xml_and_dump.py
@@ -0,0 +1,37 @@
+# App to read supporters and dump the full JSON buffer in a useful format.
+import argparse
+import requests
+import yaml
+import json
+
+# Get the login credentials
+parser = argparse.ArgumentParser(description='Read supporters')
+parser.add_argument('--login', dest='loginFile', action='store',
+                    help='YAML file with login credentials')
+
+args = parser.parse_args()
+cred = yaml.load(open(args.loginFile))
+
+# Authenticate
+payload = {
+    'email': cred['email'],
+    'password': cred['password'],
+    'json': True }
+s = requests.Session()
+u = 'https://' + cred['host'] + '/api/authenticate.sjs'
+r = s.get(u, params=payload)
+j = r.json()
+if j['status'] == 'error':
+    print('Authentication failed: ', j)
+    exit(1)
+
+# Read the HTML content from the email blast victim.
+payload = {'object': 'supporter',
+    'condition': 'First_Name=Bob',
+    'xml': True}
+u = 'https://'+ cred['host'] +'/api/getObjects.sjs'
+r = s.get(u, params=payload)
+print(r.content)
+
+# Do you want to traverse the XML?  Yes?
+# Cool! Click here https://www.geeksforgeeks.org/xml-parsing-python/
