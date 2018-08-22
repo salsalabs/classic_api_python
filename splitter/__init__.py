e7b6796dabb321b262a8be0af2c0c4e1d327080f
Allen Leonard (Salsalabs) <aleonard@salsalabs.com>
2018-08-22 15:37:36 +0000

    First pass at DOCSTRINGs.

---
+++ splitter/__init__.py
@@ -0,0 +1,37 @@
+"""Export Salsa Classic data to Engage.
+
+This package is an application to export Salsa Classic data in a form that can
+easily be imported into Salsa Engage.  
+
+The general process is to read all supporters, filter out the ones that don't
+need to be exported, then export the rest as a CSV.  This app also exports 
+donations made by the supporters groups and the supporter belonged to.
+
+The application starts from the command line.  The user provides a file
+containing Salsa campaign manager credentials.  The user may also set
+the directory where CSV files will be written.
+
+python3 splitter/app.py --help
+usage: app.py [-h] [--login LOGINFILE] [--dir OUTPUTDIR]
+
+Export supporters, groups and donations for export to Engage.
+
+optional arguments:
+  -h, --help         show this help message and exit
+  --login LOGINFILE  YAML file with login credentials
+  --dir OUTPUTDIR    Store export files here
+
+ - LOGINFILE is a YAML formatted file that holds the credentials necessary
+   to use the Salsa Classic API.  Here's an example.
+
+host: salsa4.salsalabs.com
+email: campaign-manager@whatever.com
+password: Extra-super-secret-password
+
+-OUTPUTDIR will be created if it does not already exist.
+
+Note:
+
+This is a Python version 3 app.  It will *not* run using Python version 2.
+
+"""
--- splitter/app.py
+++ splitter/app.py
@@ -1,7 +1,7 @@
-
-# Application to read all supporters, filter out the ones that don't need to be
+"""Application to read all supporters, filter out the ones that don't need to be
 # exported, then export the rest as a CSV.  This app also exports donations, groups,
-# actions and events for the supporters.
+# actions and events for the supporters."""
+
 import argparse
 import requests
 import time
@@ -15,7 +15,43 @@ from supporter import SupporterReader, SupporterSaver
 
 
 def main():
-    # The app starts here.
+    """ The app starts a supporter reader and several listeners.  The reader
+    retrieves qualified supporters from Salsa.  Each supporter is then passed
+    to one of the first-tier listeners.
+    
+    * supporter listener -- accepts a supporter and writes to the supporter
+      CSV file.
+    groups listener - accepts a supporter and passes on (group_name, email)
+      records to the second-tier groups-email listener
+    * donation listener -- accepts a supporter and passes donation record to the
+      second-tier donation-saver listener
+      
+    The second-tier listeners write to CSV files.ArithmeticError
+    
+    * groups-email listener -- accepts a (group_name, email) record and writes it 
+      to the groups CSV file
+    * donation save listener -- accepts a donation record and writes it to the 
+      donation CSV file.
+
+    The application starts from the command line.  The user provides a file
+    containing Salsa campaign manager credentials.  The user may also set
+    the directory where CSV files will be written.
+    
+     python3 splitter/app.py --help
+    usage: app.py [-h] [--login LOGINFILE] [--dir OUTPUTDIR]
+    
+    Export supporters, groups and donations for export to Engage.
+    
+    optional arguments:
+      -h, --help         show this help message and exit
+      --login LOGINFILE  YAML file with login credentials
+      --dir OUTPUTDIR    Store export files here
+ 
+    Note:
+    
+    This application *will not* run with python2.  Use python3.
+    """
+    
     exitFlag = False
     supporterQueue = LockedQueue()
     groupsQueue = LockedQueue()
@@ -25,7 +61,7 @@ def main():
     threads = []
 
     # Get the login credentials
-    parser = argparse.ArgumentParser(description='Read supporters')
+    parser = argparse.ArgumentParser(description='Export supporters, groups and donations for export to Engage.')
     parser.add_argument('--login', dest='loginFile', action='store',
                         help='YAML file with login credentials')
     parser.add_argument('--dir', dest='outputDir', action='store', default='./data',
