98fff39b25df6fc083e67a7129336abee24bf193
Allen Leonard <aleonard@salsalabs.com>
2018-10-08 17:37:31 -0500

    Compare dates, write to CSV.

--- blast_tag_ch_zero.py
+++ blast_tag_ch_zero.py
@@ -1,101 +1,147 @@
-# App to read read "tags" for supporters that have an "email_blast" tag and
-# chapter_KEY of zero.  Use the "table_KEY" value to see if there is a matching
-# unsubscribe record.  Write some output.  We'll see if the output indicates
-# a pattern.
 import argparse
-import requests
-import yaml
+import csv 
+import datetime
 import json
 import multidict
-import csv 
+import requests
+import yaml
 
-# Get the login credentials
-parser = argparse.ArgumentParser(description='Read supporters')
-parser.add_argument('--login', dest='loginFile', action='store',
-                    help='YAML file with login credentials')
-parser.add_argument('--csv', dest='csvFile', action='store', default="tags.csv",
-                    help="CSV file to contain the output records")
-args = parser.parse_args()
-cred = yaml.load(open(args.loginFile))
-writer = None
+def build_url(cred, payload):
+    # Builds the current URL using the credentials and payload.
+    u = 'https://'+ cred['host'] +'/api/getLeftJoin.sjs'
+    f1 = '{}?{}={}'
+    f2 = '{}&{}={}'
+    for k,n in payload.items():
+        if k == 'json':
+            u = f1.format(u, k, n)
+        else:
+            u = f2.format(u, k, n)
+    return u
 
-# Authenticate
-payload = {
-    'email': cred['email'],
-    'password': cred['password'],
-    'json': True }
-s = requests.Session()
-u = 'https://' + cred['host'] + '/api/authenticate.sjs'
-r = s.get(u, params=payload)
-j = r.json()
-if j['status'] == 'error':
-    print('Authentication failed: ', j)
-    exit(1)
+def parse_date(x):
+    # Parse Salsa Classic dates into a datetime.
+    # Input is like "Mon Oct 09 2017 19:25:56 GMT-0400"
+    # Output is a datetime object.
+    x = x.split(" GMT")[0].strip()
+    f = '%a %b %d %Y %X'
+    return datetime.datetime.strptime(x, f)
 
-print('Authentication: ', j)
+def handle(j, csvFN, writer):
+    # Process the contents of the JSON object.  Creates
+    # a CSV writer as needed.  Writes contents to file
+    # "csvFN".  Returns the writer.
 
-conditions = [
-    "tag.prefix=email_blast",
-    "tag_data.database_table_KEY=142",
-    "tag_data.chapter_KEY=0",
-]
-includes = [
-    "tag.prefix",
-    "tag.tag",
-    "tag_data.table_KEY",
-    "tag_data.Last_Modified",
-    "supporter_KEY",
-    "Unsubscribe_Date"
-]
-payload = multidict.MultiDict([
-    ('json', True),
-    ('object', 'unsubscribe(supporter_KEY=tag_data.table_KEY)tag_data(tag_KEY)tag'),
-    ('condition', 'tag.prefix=email_blast'),
-    ('condition',  'tag_data.database_table_KEY=142'),
-    ('condition', 'tag_data.chapter_KEY=0'),
-    ('include', ','.join(includes))
-])
-print(payload)
-u = 'https://'+ cred['host'] +'/api/getLeftJoin.sjs'
+    print("Found ", len(j), ' records.')
+    # Iterate through the hashes and display essential parts of the record.
+    f = '{:10}:{:10} {:10} {:20} {:20} {:20} {:5}'
+    for tag in j:
+        tag["interesting"] = False
+        if writer == None:
+            heads = 'prefix,tag,table_KEY,Last_Modified,Unsubscribe_Date,interesting'.split(",")
+            csvFile = open(args.csvFN, 'w')
+            writer = csv.DictWriter(csvFile, heads)
 
-#Passing MultiDict to requests doesn't seem to work.
-#We'll fake it by creating a really long URL because HTTP GET.
+        # If the unsub time is less than the tag's last modified, then
+        # that suggests that the chapter_KEY of zero is legitimate.
 
-f1 = '{}?{}={}'
-f2 = '{}&{}={}'
-for k,n in payload.items():
-    if k == 'json':
-        u = f1.format(u, k, n)
-    else:
-        u = f2.format(u, k, n)
-r = s.get(u)
+        format='%c GMT%z'
+        t1 = parse_date(tag["Unsubscribe_Date"])
+        t2 = parse_date(tag["Last_Modified"])
+        tag["interesting"] = t1 < t2
 
-print(r.url)
-print(r.status_code)
-print(r.text)
+        print(f.format(tag["prefix"],
+        tag["tag"],
+        tag["table_KEY"],
+        tag["Last_Modified"],
+        tag["Unsubscribe_Date"],
+        tag["interesting"]))
+        writer.writerow(tag)
 
-# This automatcally converts the JSON buffer to an array of dictionaries.
-j = r.json()
-if j.status_code != 200:
-    print("\nRead error")
-    print("Status code:", j.status_code)
-    print("Body:", j.text)
-    exit(0)
+    return writer
 
-print("Found ", len(j), ' records.')
-# Iterate through the hashes and display essential parts of the record.
-f = '{:10}:{:10} {:10} {:20} {:20} {:20} {:20}å'
-for tag in j:
-    if writer == None:
-        writer = csv.DictWriter(open(args.csvFile, 'w'), tag.keys())
-    print(f.format(tag["prefix"],
-     tag["tag"],
-     tag["table_KEY"],
-     tag["chapter_KEY"],
-     tag["Last_Modified"],
-     tag["supporter_KEY"],
-     tag["Unsubscribe_Data"]))
-    writer.writeDict(j)
-writer.close()
-print("Done")
+def main():
+    # App to read read "tags" for supporters that have an "email_blast" tag and
+    # chapter_KEY of zero.  Use the "table_KEY" value to see if there is a matching
+    # unsubscribe record.  Write some output.  We'll see if the output indicates
+    # a pattern.
 
+    # Get the login credentials
+    parser = argparse.ArgumentParser(description='Read supporters')
+    parser.add_argument('--login', dest='loginFile', action='store',
+                        help='YAML file with login credentials')
+    parser.add_argument('--csv', dest='csvFN', action='store', default="tags.csv",
+                        help="CSV file to contain the output records")
+    args = parser.parse_args()
+    cred = yaml.load(open(args.loginFile))
+    writer = None
+    csvFile = None
+
+    # Authenticate
+    payload = {
+        'email': cred['email'],
+        'password': cred['password'],
+        'json': True }
+    s = requests.Session()
+    u = 'https://' + cred['host'] + '/api/authenticate.sjs'
+    r = s.get(u, params=payload)
+    j = r.json()
+    if j['status'] == 'error':
+        print('Authentication failed: ', j)
+        exit(1)
+
+    print('Authentication: ', j['status'])
+
+    conditions = [
+        "tag.prefix=email_blast",
+        "tag_data.database_table_KEY=142",
+        "tag_data.chapter_KEY=0",
+    ]
+    includes = [
+        "tag.prefix",
+        "tag.tag",
+        "tag_data.table_KEY",
+        "tag_data.Last_Modified",
+        "Unsubscribe_Date"
+    ]
+
+    # We're using MultiDict because it is is more expressive
+    # than a list of things. Also easier to relate back to 
+    # the API doc.
+    payload = multidict.MultiDict([
+        ('json', True),
+        ('object', 'unsubscribe(supporter_KEY=tag_data.table_KEY)tag_data(tag_KEY)tag'),
+        ('condition', 'tag.prefix=email_blast'),
+        ('condition',  'tag_data.database_table_KEY=142'),
+        ('condition', 'tag_data.chapter_KEY=0'),
+        ('include', ','.join(includes),
+        ('count', 500),
+        ('offset', 0)
+    ])
+    #print(payload)
+
+    # But... passing MultiDict to requests doesn't seem to work.
+    # We'll fake it by creating a really long URL because HTTP GET.
+
+    while payload['count'] > 0:
+        f = 'Reading {} from offset {}\n'
+        print(f.format(payload['count'], payload['offset']))
+        print()
+        u = build_url(cred, payload)
+        r = s.get(u)
+
+        if r.status_code != 200:
+            print("\nRead error")
+            print("Status code:", j.status_code)
+            print("Body:", j.text)
+            print()
+            exit(0)
+
+        # This automatcally converts the JSON buffer to an array of dictionaries.
+        j = r.json()
+        if len(j) > 0
+            writer = handle(j, csvFN, writer)
+        payload['count'] = len(j)
+        payload['offset'] = payload['offset'] + payload['count']
+    print("Done")
+
+main()
